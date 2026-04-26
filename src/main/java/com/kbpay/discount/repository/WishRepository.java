cfec42e1c8022cd232e0e37c2faab199023aa009
dobe1004 <dobe1004@gmail.com>
2026-04-27 00:21:39 +0900

    Update WishRepository.java

--- src/main/java/com/kbpay/discount/repository/WishRepository.java
+++ src/main/java/com/kbpay/discount/repository/WishRepository.java
@@ -3,6 +3,7 @@ package com.kbpay.discount.repository;
 import com.kbpay.discount.domain.Wish;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.*;
 
 @Repository
@@ -10,5 +11,6 @@ public interface WishRepository extends JpaRepository<Wish, Long> {
     List<Wish> findByUserIdOrderByCreatedAtDesc(String userId);
     Optional<Wish> findByUserIdAndProductId(String userId, Long productId);
     boolean existsByUserIdAndProductId(String userId, Long productId);
+    @Transactional
     void deleteByUserIdAndProductId(String userId, Long productId);
 }
